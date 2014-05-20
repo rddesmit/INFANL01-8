cbae445a434a7956dd92956e7888f64379d6b273
Rudie de Smit <rddesmit@hotmail.com>
2014-05-21 00:12:18 +0200

    Created test that are not dependent on test order.

--- src/main/Database.java
+++ src/main/Database.java
@@ -28,7 +28,6 @@ public class Database {
         try {
             Statement statement = connection.createStatement();
             result = statement.executeUpdate(query);
-            System.out.println(result);
             statement.close();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -38,6 +37,82 @@ public class Database {
             return false;
         }
         return true;
+    }
+
+    private void insertDelete(String query){
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute(query);
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void cleanDatabase(){
+        deleteData();
+        insertData();
+    }
+
+    private void deleteData(){
+        insertDelete("DELETE FROM klassen;");
+        insertDelete("DELETE FROM adressen;");
+        insertDelete("DELETE FROM personen;");
+        insertDelete("DELETE FROM studenten;");
+        insertDelete("DELETE FROM deelnemers;");
+        insertDelete("DELETE FROM docenten;");
+        insertDelete("DELETE FROM modules;");
+        insertDelete("DELETE FROM lestijden;");
+        insertDelete("DELETE FROM roosters;");
+        insertQuery("SELECT setval(pg_get_serial_sequence('personen', 'persoonsnummer'), 1);");
+    }
+
+    private void insertData(){
+        insertQuery("INSERT INTO klassen VALUES ('INF2D', '1-1-2014', '31-12-2014');");
+        insertQuery("INSERT INTO klassen VALUES ('INF3D', '1-1-2015', '31-12-2015');");
+        insertQuery("INSERT INTO klassen VALUES ('INF25D', '1-7-2014', '30-6-2015');");
+
+        insertQuery("INSERT INTO adressen VALUES ('3295ga', '''s-Gravendeel', 'Geuldam');");
+        insertQuery("INSERT INTO adressen VALUES ('2957 JB', 'Nieuw-Lekkerland', 'Watervluchtmolen');");
+        insertQuery("INSERT INTO adressen VALUES ('3011WN', 'Rotterdam', 'Wijnhaven');");
+
+        insertQuery("INSERT INTO personen (voornaam, achternaam, tussenvoegsel, geboorte_datum, geslacht, postcode, huisnummer, telefoonnummer) VALUES ('Paul', 'Wols', '', '7-8-1990', 'man', '3295GA', '16', '(06) - 0606');");
+        insertQuery("INSERT INTO personen (voornaam, achternaam, tussenvoegsel, geboorte_datum, geslacht, postcode, huisnummer, telefoonnummer) VALUES ('Rudie', 'Smit', 'de', '1-1-1993', 'man', '2957JB', '45', '(06) - 21322121');");
+        insertQuery("INSERT INTO personen (voornaam, achternaam, tussenvoegsel, geboorte_datum, geslacht, postcode, huisnummer, telefoonnummer) VALUES ('Tony', 'Busker', '', '1-1-1965', 'man', '3011WN', '107', '012.456789');");
+        insertQuery("INSERT INTO personen (voornaam, achternaam, tussenvoegsel, geboorte_datum, geslacht, postcode, huisnummer, telefoonnummer) VALUES ('Wendy', 'IJperen', 'van', '1-1-1980', 'vrouw', '3011WN', '107', '012.21352547');");
+
+        insertQuery("INSERT INTO studenten VALUES ('0812345', '2');");
+        insertQuery("INSERT INTO studenten VALUES ('0854321', '3');");
+
+        insertQuery("INSERT INTO deelnemers VALUES ('0812345', 'INF2D');");
+        insertQuery("INSERT INTO deelnemers VALUES ('0812345', 'INF3D');");
+        insertQuery("INSERT INTO deelnemers VALUES ('0854321', 'INF25D');");
+
+        insertQuery("INSERT INTO docenten VALUES ('BUSAL.1', '4');");
+        insertQuery("INSERT INTO docenten VALUES ('IJPEW.1', '5');");
+
+        insertQuery("INSERT INTO modules VALUES ('INFANL', 'Analyse 8 - Advanced Database', 'BUSAL.1', '5-5-2014', '30-6-2014');");
+        insertQuery("INSERT INTO modules VALUES ('INFSKL', 'Skills - Jaar 2', 'IJPEW.1', '5-5-2014', '30-6-2014');");
+
+        insertQuery("INSERT INTO lestijden VALUES (1, '08:30');");
+        insertQuery("INSERT INTO lestijden VALUES (2, '09:20');");
+        insertQuery("INSERT INTO lestijden VALUES (3, '10:30');");
+        insertQuery("INSERT INTO lestijden VALUES (4, '11:20');");
+        insertQuery("INSERT INTO lestijden VALUES (5, '12:10');");
+        insertQuery("INSERT INTO lestijden VALUES (6, '13:00');");
+        insertQuery("INSERT INTO lestijden VALUES (7, '13:50');");
+        insertQuery("INSERT INTO lestijden VALUES (8, '15:00');");
+        insertQuery("INSERT INTO lestijden VALUES (9, '15:50');");
+        insertQuery("INSERT INTO lestijden VALUES (10, '17:00');");
+        insertQuery("INSERT INTO lestijden VALUES (11, '17:50');");
+        insertQuery("INSERT INTO lestijden VALUES (12, '18:40');");
+        insertQuery("INSERT INTO lestijden VALUES (13, '19:30');");
+        insertQuery("INSERT INTO lestijden VALUES (14, '20:20');");
+        insertQuery("INSERT INTO lestijden VALUES (15, '21:10');");
+
+        insertQuery("INSERT INTO roosters (klasnaam, modulecode, docent, beginuur, einduur, datum, lokaal) VALUES ('INF2D', 'INFANL', 'BUSAL.1', 2, 4, '5-6-2014', 'WH104.1.105');");
+        insertQuery("INSERT INTO roosters (klasnaam, modulecode, docent, beginuur, einduur, datum, lokaal) VALUES ('INF25D', 'INFSKL', 'BUSAL.1', 4, 6, '5-6-2014', 'WH103.1.105');");
+        insertQuery("INSERT INTO roosters (klasnaam, modulecode, docent, beginuur, einduur, datum, lokaal) VALUES ('INF2D', 'INFSKL', 'IJPEW.1', 4, 6, '5-6-2014', 'WH104.1.105');");
     }
 
     public void disconnect() throws SQLException {
--- src/test/MainTest.java
+++ src/test/MainTest.java
@@ -20,21 +20,24 @@ public class MainTest{
 
     @Test
     public void testKlassen() {
-        assertTrue(database.insertQuery("INSERT INTO klassen VALUES ('INF2D', '1-1-2014', '31-12-2014');"));
-        assertTrue(database.insertQuery("INSERT INTO klassen VALUES ('INF3D', '1-1-2015', '31-12-2015');"));
-        assertTrue(database.insertQuery("INSERT INTO klassen VALUES ('INF25D', '1-7-2014', '30-6-2015');"));
+        database.cleanDatabase();
+        assertTrue(database.insertQuery("INSERT INTO klassen VALUES ('INF4D', '1-1-2014', '31-12-2014');"));
+        assertTrue(database.insertQuery("INSERT INTO klassen VALUES ('INF3E', '1-1-2015', '31-12-2015');"));
+        assertTrue(database.insertQuery("INSERT INTO klassen VALUES ('INF26D', '1-7-2014', '30-6-2015');"));
     }
 
     @Test
     public void testAddressen(){
-        assertTrue(database.insertQuery("INSERT INTO adressen VALUES ('3295ga', '''s-Gravendeel', 'Geuldam');"));
-        assertTrue(database.insertQuery("INSERT INTO adressen VALUES ('2957 JB', 'Nieuw-Lekkerland', 'Watervluchtmolen');"));
+        database.cleanDatabase();
+        assertTrue(database.insertQuery("INSERT INTO adressen VALUES ('3296ga', '''s-Gravendeel', 'Geuldam');"));
+        assertTrue(database.insertQuery("INSERT INTO adressen VALUES ('2947 JB', 'Nieuw-Lekkerland', 'Watervluchtmolen');"));
         assertFalse(database.insertQuery("INSERT INTO adressen VALUES ('295721', 'Nieuw-Lekkerland', 'Watervluchtmolen');"));
-        assertTrue(database.insertQuery("INSERT INTO adressen VALUES ('3011WN', 'Rotterdam', 'Wijnhaven');"));
+        assertTrue(database.insertQuery("INSERT INTO adressen VALUES ('3012WN', 'Rotterdam', 'Wijnhaven');"));
     }
 
     @Test
     public void testPersonen(){
+        database.cleanDatabase();
         assertTrue(database.insertQuery("INSERT INTO personen (voornaam, achternaam, tussenvoegsel, geboorte_datum, geslacht, postcode, huisnummer, telefoonnummer) VALUES ('Paul', 'Wols', '', '7-8-1990', 'man', '3295GA', '16', '(06) - 0606');"));
         assertTrue(database.insertQuery("INSERT INTO personen (voornaam, achternaam, tussenvoegsel, geboorte_datum, geslacht, postcode, huisnummer, telefoonnummer) VALUES ('Rudie', 'Smit', 'de', '1-1-1993', 'man', '2957JB', '45', '(06) - 21322121');"));
         assertFalse(database.insertQuery("INSERT INTO personen (voornaam, achternaam, tussenvoegsel, geboorte_datum, geslacht, postcode, huisnummer, telefoonnummer) VALUES ('Piet', 'Verdriet', '', '1-1-1900', 'man', '2957JB', '45', '(06) - aaaa ');"));
@@ -45,63 +48,58 @@ public class MainTest{
 
     @Test
     public void testStudenten(){
-        assertTrue(database.insertQuery("INSERT INTO studenten VALUES ('0812345', '1');"));
-        assertTrue(database.insertQuery("INSERT INTO studenten VALUES ('0854321', '2');"));
-        assertFalse(database.insertQuery("INSERT INTO studenten VALUES ('0811111', '1');"));
+        database.cleanDatabase();
+        assertTrue(database.insertQuery("INSERT INTO studenten VALUES ('0812344', '4');"));
+        assertTrue(database.insertQuery("INSERT INTO studenten VALUES ('0854322', '5');"));
+        assertFalse(database.insertQuery("INSERT INTO studenten VALUES ('0811112', '4');"));
     }
 
     @Test
     public void testDeelnemers(){
-        assertTrue(database.insertQuery("INSERT INTO deelnemers VALUES ('0812345', 'INF2D');"));
-        assertTrue(database.insertQuery("INSERT INTO deelnemers VALUES ('0812345', 'INF3D');"));
-        assertFalse(database.insertQuery("INSERT INTO deelnemers VALUES ('0812345', 'INF25D');"));
-        assertTrue(database.insertQuery("INSERT INTO deelnemers VALUES ('0854321', 'INF25D');"));
-        assertFalse(database.insertQuery("INSERT INTO deelnemers VALUES ('0854321', 'INF2D');"));
-        assertFalse(database.insertQuery("INSERT INTO deelnemers VALUES ('0854321', 'INF3D');"));
+        database.cleanDatabase();
+        database.insertQuery("INSERT INTO klassen VALUES ('INF4D', '1-1-2016', '31-12-2017');");
+        database.insertQuery("INSERT INTO klassen VALUES ('INF3E', '1-1-2018', '31-12-2019');");
+        database.insertQuery("INSERT INTO klassen VALUES ('INF25E', '1-7-2015', '30-6-2016');");
+
+        assertTrue(database.insertQuery("INSERT INTO deelnemers VALUES ('0812345', 'INF4D');"));
+        assertTrue(database.insertQuery("INSERT INTO deelnemers VALUES ('0812345', 'INF3E');"));
+        assertFalse(database.insertQuery("INSERT INTO deelnemers VALUES ('0812345', 'INF25E');"));
+        assertTrue(database.insertQuery("INSERT INTO deelnemers VALUES ('0854321', 'INF25E');"));
+        assertFalse(database.insertQuery("INSERT INTO deelnemers VALUES ('0854321', 'INF4D');"));
     }
 
     @Test
     public void testDocenten(){
-        assertTrue(database.insertQuery("INSERT INTO docenten VALUES ('BUSAL.1', '4');"));
-        assertFalse(database.insertQuery("INSERT INTO docenten VALUES ('BUSAL.2', '4');"));
-        assertTrue(database.insertQuery("INSERT INTO docenten VALUES ('IJPEW.1', '5');"));
+        database.cleanDatabase();
+        assertTrue(database.insertQuery("INSERT INTO docenten VALUES ('BUSAL.3', '2');"));
+        assertFalse(database.insertQuery("INSERT INTO docenten VALUES ('BUSAL.4', '2');"));
+        assertTrue(database.insertQuery("INSERT INTO docenten VALUES ('IJPEW.3', '3');"));
     }
 
     @Test
     public void testModules(){
-        assertTrue(database.insertQuery("INSERT INTO modules VALUES ('INFANL', 'Analyse 8 - Advanced Database', 'BUSAL.1', '5-5-2014', '30-6-2014');"));
-        assertTrue(database.insertQuery("INSERT INTO modules VALUES ('INFSKL', 'Skills - Jaar 2', 'IJPEW.1', '5-5-2014', '30-6-2014');"));
-        assertFalse(database.insertQuery("INSERT INTO modules VALUES ('INFDEV01', 'Development 1 - HTML, CSS en Javascript', 'BUSAL.1', '5-5-2014', '31-7-2014');"));
-    }
+        database.cleanDatabase();
+        database.insertQuery("INSERT INTO docenten VALUES ('BUSAL.3', '2');");
+        database.insertQuery("INSERT INTO docenten VALUES ('IJPEW.3', '3');");
 
-    @Test
-    public void testLestijden(){
-        assertTrue(database.insertQuery("INSERT INTO lestijden VALUES (1, '08:30');"));
-        assertTrue(database.insertQuery("INSERT INTO lestijden VALUES (2, '09:20');"));
-        assertTrue(database.insertQuery("INSERT INTO lestijden VALUES (3, '10:30');"));
-        assertTrue(database.insertQuery("INSERT INTO lestijden VALUES (4, '11:20');"));
-        assertTrue(database.insertQuery("INSERT INTO lestijden VALUES (5, '12:10');"));
-        assertTrue(database.insertQuery("INSERT INTO lestijden VALUES (6, '13:00');"));
-        assertTrue(database.insertQuery("INSERT INTO lestijden VALUES (7, '13:50');"));
-        assertTrue(database.insertQuery("INSERT INTO lestijden VALUES (8, '15:00');"));
-        assertTrue(database.insertQuery("INSERT INTO lestijden VALUES (9, '15:50');"));
-        assertTrue(database.insertQuery("INSERT INTO lestijden VALUES (10, '17:00');"));
-        assertTrue(database.insertQuery("INSERT INTO lestijden VALUES (11, '17:50');"));
-        assertTrue(database.insertQuery("INSERT INTO lestijden VALUES (12, '18:40');"));
-        assertTrue(database.insertQuery("INSERT INTO lestijden VALUES (13, '19:30');"));
-        assertTrue(database.insertQuery("INSERT INTO lestijden VALUES (14, '20:20');"));
-        assertTrue(database.insertQuery("INSERT INTO lestijden VALUES (15, '21:10');"));
+        assertTrue(database.insertQuery("INSERT INTO modules VALUES ('INFANLX', 'Analyse 8 - Advanced Database', 'BUSAL.3', '5-5-2014', '30-6-2014');"));
+        assertTrue(database.insertQuery("INSERT INTO modules VALUES ('INFSKLX', 'Skills - Jaar 2', 'IJPEW.3', '5-5-2014', '30-6-2014');"));
+        assertFalse(database.insertQuery("INSERT INTO modules VALUES ('INFDEV01X', 'Development 1 - HTML, CSS en Javascript', 'BUSAL.3', '5-5-2014', '31-7-2014');"));
     }
 
     @Test
     public void testRoosters(){
-        assertFalse(database.insertQuery("INSERT INTO roosters (klasnaam, modulecode, docent, beginuur, einduur, datum, lokaal) VALUES ('INF25D', 'INFANL', 'BUSAL.1', 1, 3, '5-7-2014', 'WH103.1.105');"));
-        assertTrue(database.insertQuery("INSERT INTO roosters (klasnaam, modulecode, docent, beginuur, einduur, datum, lokaal) VALUES ('INF2D', 'INFANL', 'BUSAL.1', 2, 4, '5-6-2014', 'WH104.1.105');"));
-        assertFalse(database.insertQuery("INSERT INTO roosters (klasnaam, modulecode, docent, beginuur, einduur, datum, lokaal) VALUES ('INF3D', 'INFSKL', 'BUSAL.1', 3, 5, '5-6-2014', 'WH103.1.105');"));
-        assertFalse(database.insertQuery("INSERT INTO roosters (klasnaam, modulecode, docent, beginuur, einduur, datum, lokaal) VALUES ('INF2D', 'INFSKL', 'IJPEW.1', 1, 3, '5-6-2014', 'WH103.1.105');"));
-        assertFalse(database.insertQuery("INSERT INTO roosters (klasnaam, modulecode, docent, beginuur, einduur, datum, lokaal) VALUES ('INF3D', 'INFSKL', 'IJPEW.1', 1, 3, '5-6-2014', 'WH104.1.105');"));
-        assertTrue(database.insertQuery("INSERT INTO roosters (klasnaam, modulecode, docent, beginuur, einduur, datum, lokaal) VALUES ('INF25D', 'INFSKL', 'BUSAL.1', 4, 6, '5-6-2014', 'WH103.1.105');"));
-        assertTrue(database.insertQuery("INSERT INTO roosters (klasnaam, modulecode, docent, beginuur, einduur, datum, lokaal) VALUES ('INF2D', 'INFSKL', 'IJPEW.1', 4, 6, '5-6-2014', 'WH104.1.105');"));
+        database.cleanDatabase();
+        database.insertQuery("INSERT INTO modules VALUES ('INFANLX', 'Analyse 8 - Advanced Database', 'BUSAL.1', '5-5-2015', '30-6-2015');");
+        database.insertQuery("INSERT INTO modules VALUES ('INFSKLX', 'Skills - Jaar 2', 'IJPEW.1', '5-5-2015', '30-6-2015');");
+
+        assertFalse(database.insertQuery("INSERT INTO roosters (klasnaam, modulecode, docent, beginuur, einduur, datum, lokaal) VALUES ('INF25D', 'INFANLX', 'BUSAL.1', 1, 3, '5-7-2015', 'WH103.1.105');"));
+        assertTrue(database.insertQuery("INSERT INTO roosters (klasnaam, modulecode, docent, beginuur, einduur, datum, lokaal) VALUES ('INF2D', 'INFANLX', 'BUSAL.1', 2, 4, '5-6-2015', 'WH104.1.105');"));
+        assertFalse(database.insertQuery("INSERT INTO roosters (klasnaam, modulecode, docent, beginuur, einduur, datum, lokaal) VALUES ('INF3D', 'INFSKLX', 'BUSAL.1', 3, 5, '5-6-2015', 'WH103.1.105');"));
+        assertFalse(database.insertQuery("INSERT INTO roosters (klasnaam, modulecode, docent, beginuur, einduur, datum, lokaal) VALUES ('INF2D', 'INFSKLX', 'IJPEW.1', 1, 3, '5-6-2015', 'WH103.1.105');"));
+        assertFalse(database.insertQuery("INSERT INTO roosters (klasnaam, modulecode, docent, beginuur, einduur, datum, lokaal) VALUES ('INF3D', 'INFSKLX', 'IJPEW.1', 1, 3, '5-6-2015', 'WH104.1.105');"));
+        assertTrue(database.insertQuery("INSERT INTO roosters (klasnaam, modulecode, docent, beginuur, einduur, datum, lokaal) VALUES ('INF25D', 'INFSKLX', 'BUSAL.1', 4, 6, '5-6-2015', 'WH103.1.105');"));
+        assertTrue(database.insertQuery("INSERT INTO roosters (klasnaam, modulecode, docent, beginuur, einduur, datum, lokaal) VALUES ('INF2D', 'INFSKLX', 'IJPEW.1', 4, 6, '5-6-2015', 'WH104.1.105');"));
     }
 
     @After
